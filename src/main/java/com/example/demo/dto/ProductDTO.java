b3bea61ca95c8768a5e3eae42f289a2c504bec8a
GOEUN PARK <dbtshfdl1@gmail.com>
2023-05-17 02:16:42 +0900

    400 오류 해결 - title 검색 기능 - title 삭제 기능

---
+++ src/main/java/com/example/demo/dto/ProductDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.ProductEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductDTO {
+    private String id;
+    private String title;
+    private String maker;
+    private String color;
+
+    public ProductDTO(final ProductEntity entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.maker = entity.getMaker();
+        this.color = entity.getColor();
+    }
+
+    public static ProductEntity toEntity(ProductDTO dto) {
+        return ProductEntity.builder()
+                .id(dto.getId())
+                .title(dto.getTitle())
+                .maker(dto.getMaker())
+                .color(dto.getColor())
+                .build();
+    }
+}
---
+++ src/main/java/com/example/demo/persistence/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.persistence;
+
+import com.example.demo.model.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+
+    //@Query(value = "select * from Todo t where t.userid = ?1", nativeQuery = true)
+    List<ProductEntity> findByUserId(String userId);
+    Optional<ProductEntity> findByTitleContaining(String title);
+}
